5b3cd3e6cb7868abe4c04075d2d41f28495a585a
nitish-rider <nitishks1110@gmail.com>
2022-01-18 16:09:25 +0530

    Register UI Done

--- app/src/main/java/com/xboard/xboardandroid/MainActivity.kt
+++ app/src/main/java/com/xboard/xboardandroid/MainActivity.kt
@@ -11,6 +11,10 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.xboard.xboardandroid.Utils.API.api
 import com.xboard.xboardandroid.Utils.CONSTANTS.channelEvent
 import org.javacord.api.DiscordApi
@@ -19,39 +23,48 @@ import kotlin.random.Random
 
 @RequiresApi(Build.VERSION_CODES.N)
 class MainActivity : AppCompatActivity() {
-    var otp: String = ""
-    var i=0
-    override fun onDestroy() {
-        super.onDestroy()
-        api.disconnect()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        api.disconnect()
-    }
+//    var otp: String = ""
+//    var i=0
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        api.disconnect()
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        api.disconnect()
+//    }
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println(api.channels)
-        for(cha in api.channels){
-            Log.d("event",cha.toString())
-        }
+
+        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+
+
+//        println(api.channels)
+//        for(cha in api.channels){
+//            Log.d("event",cha.toString())
+//        }
+
+
+
+
 //        onReady(api)
         //930088852826234970
 
-        var channel=api.getTextChannelById("840446893216497726")
-        channel.ifPresent { textChannel->
-            textChannel.sendMessage("4298")
-            var chat=textChannel.getMessages(10).thenApply {chatSet->
-             for(singleChat in chatSet){
-                 Log.d("event",singleChat.toString())
-             }
-            }
-
-        }
+//        var channel=api.getTextChannelById("840446893216497726")
+//        channel.ifPresent { textChannel->
+////            textChannel.sendMessage("4298")
+//            var chat=textChannel.getMessages(10).thenApply{chatSet->
+//             for(singleChat in chatSet){
+//                 Log.d("event",singleChat.toString())
+//             }
+//            }
+//
+//        }
 
 //        api.addMessageCreateListener {event->
 //            if(event.messageContent.equals(otp)){
@@ -68,24 +81,29 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun onReady(api: DiscordApi) {
-        otp = Random.nextInt(1000, 9999).toString()
-        val dialog= Dialog(this)
-        dialog.setContentView(R.layout.alert_dialog)
-        dialog.findViewById<TextView>(R.id.otp).text=otp
-        dialog.findViewById<Button>(R.id.dialogButtonCopy).setOnClickListener {
-            val clipBoardManager: ClipboardManager?= ContextCompat.getSystemService(
-                this,
-                CLIPBOARD_SERVICE::class.java
-            ) as ClipboardManager?
-            val clipData= ClipData.newPlainText("otp",otp)
-            clipBoardManager?.setPrimaryClip(clipData)
-            dialog.dismiss()
-            onLogin()
-        }
-        dialog.show()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController=findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()||super.onSupportNavigateUp()
     }
-    fun onLogin(){
-        channelEvent?.channel?.sendMessage("Hello There!!!")
-    }
+
+//    fun onReady(api: DiscordApi) {
+//        otp = Random.nextInt(1000, 9999).toString()
+//        val dialog= Dialog(this)
+//        dialog.setContentView(R.layout.alert_dialog)
+//        dialog.findViewById<TextView>(R.id.otp).text=otp
+//        dialog.findViewById<Button>(R.id.dialogButtonCopy).setOnClickListener {
+//            val clipBoardManager: ClipboardManager?= ContextCompat.getSystemService(
+//                this,
+//                CLIPBOARD_SERVICE::class.java
+//            ) as ClipboardManager?
+//            val clipData= ClipData.newPlainText("otp",otp)
+//            clipBoardManager?.setPrimaryClip(clipData)
+//            dialog.dismiss()
+//            onLogin()
+//        }
+//        dialog.show()
+//    }
+//    fun onLogin(){
+//        channelEvent?.channel?.sendMessage("Hello There!!!")
+//    }
 }
--- app/src/main/java/com/xboard/xboardandroid/Utils/CONSTANTS.kt
+++ app/src/main/java/com/xboard/xboardandroid/Utils/CONSTANTS.kt
@@ -4,7 +4,7 @@ import org.javacord.api.entity.channel.TextChannel
 import org.javacord.api.event.message.MessageCreateEvent
 
 object CONSTANTS {
-    const val BOT_KEY="OTI4Mjc3NDU2ODc0NTMyODg1.YdWbwA.OTdMaAJpnRJf5Ugb7fgzvLt61q4"
+    const val BOT_KEY="OTI4Mjc3NDU2ODc0NTMyODg1.YdWbwA.wgUopUu0kYe6xgo1Xpg_hsoy5RU"
     var Channel_ID=""
     var channelEvent: MessageCreateEvent? =null
 }
